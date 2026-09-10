0dc8224af6f85cd6766ab2692caf6446ffbcc22a
PrathmeshDesai <desaiprathmesh21@gmail.com>
2026-09-11 00:31:53 +0530

    chore: remove node_modules tracking and update root gitignore

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from "cors";
 
+import path from 'path';
+
 import { connectDb } from './lib/db.js';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
@@ -11,6 +13,7 @@ import { app ,server } from './lib/socket.js';
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // CORS setup
 app.use(cors({
@@ -25,6 +28,14 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE.ENV === "production"){
+    app.use(express.static(path.join(__dirname , "../frontend/dist")));
+    
+    app.get("*" , (req , res) => {
+        res.sendFile(path.join(__dirname , "../frontend" , "dist" , "index.html"));
+    })
+}
 
 server.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
