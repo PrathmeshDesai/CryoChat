f4d7869fd0ba63b9a4c39d71dbe63b705c832a15
PrathmeshDesai <desaiprathmesh21@gmail.com>
2026-09-11 01:22:49 +0530

    security: remove exposed env file from tracking

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,12 +29,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE.ENV === "production"){
-    app.use(express.static(path.join(__dirname , "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist"))); // Point up one folder from backend to root, then into frontend/dist
     
-    app.get("*" , (req , res) => {
-        res.sendFile(path.join(__dirname , "../frontend" , "dist" , "index.html"));
-    })
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
 }
 
 server.listen(PORT, async () => {
